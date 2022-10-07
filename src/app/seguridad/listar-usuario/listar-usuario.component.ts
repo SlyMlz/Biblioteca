843f74c698037864bea455379cae0b1af210d9bb
slymlz <slymlz98dta@gmail.com>
2022-10-07 17:52:39 -0500

    listar usuarios y registrar

--- src/app/seguridad/listar-usuario/listar-usuario.component.ts
+++ src/app/seguridad/listar-usuario/listar-usuario.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarUsuarioComponent implements OnInit {
 
+  listUsuario:any [] = [
+    {
+      
+      nombre:'Meliza Gomez',
+      cedula:'11227863',
+      correo:'slymlz98@gmail.com',
+      n_celular:314639984,
+    },
+
+    {
+      nombre:'Catheryn Cabrera',
+      cedula:'1234587',
+      correo:'sfab@gmail.com',
+      n_celular:3158758742,
+      
+    }, 
+
+  ]
   constructor() { }
 
   ngOnInit(): void {
